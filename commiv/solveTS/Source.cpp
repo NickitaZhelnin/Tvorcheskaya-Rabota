6181f4b415abb27a07f5e35ba9f44f1be3bdf05a
Nickita Zhelnin <130294527+NickitaZhelnin@users.noreply.github.com>
2023-06-11 22:49:13 +0500

    Update Source.cpp

--- commiv/solveTS/Source.cpp
+++ commiv/solveTS/Source.cpp
@@ -1,222 +1,166 @@
 #include <iostream>
 #include <vector>
-#include <limits>
 #include <SFML/Graphics.hpp>
 using namespace std;
 
 const int windowWidth = 800;
 const int windowHeight = 600;
 
-// путь из одного города в другой
+// РїСѓС‚СЊ РёР· РѕРґРЅРѕРіРѕ РіРѕСЂРѕРґР° РІ РґСЂСѓРіРѕР№
 struct Edge {
-    int first, second;
-    float weight;
+    int first;
+    int second;
+    int weight;
 };
 
-// поиск минимальной границы
-float findMinBound(int currentNode, int visited, vector<vector<float>>& graph) {
-    float minBound = 0;
-    for (int i = 1; i < graph.size(); i++) {
-        if (visited & (1 << i)) {
-            minBound += graph[(i - 1)][(i)];
-        }
+// РЅР°С…РѕР¶РґРµРЅРёРµ РѕС‚РІРµС‚Р°
+int solve(int cur, int h, vector<vector<int>>& matr, vector<vector<int>>& matr2, vector<int>& road) {
+    int next = -1;
+    if (matr2[cur][h] != -1) {
+        return matr2[cur][h];
     }
-    float findMinNext = numeric_limits<float>::max();
-    for (int i = 1; i < graph.size(); i++) {
-        if ((visited & (1 << i)) == 0 && graph[currentNode][i] < findMinNext) {
-            findMinNext = graph[currentNode][i];
-        }
-    }
-    float findMin2Next = numeric_limits<float>::max();
-    for (int i = 1; i < graph.size(); i++) {
-        if (visited & (1 << i)) {
-            for (int j = 1; j < graph.size(); j++) {
-                if ((visited & (1 << j)) == 0 && graph[j][i] < findMin2Next) {
-                    findMin2Next = graph[j][i];
-                }
+    if (h == ((1 << matr.size()) - 1)) {
+        road.push_back(0);
+        return matr[cur][0];
+    }  
+    int bestway = 999999;
+    for (int i = 0; i < matr.size(); i++) {
+        if ((h & (1 << i)) == 0 && i != cur) {
+            int curway = matr[cur][i] + solve(i, h | (1 << i), matr, matr2, road);
+            if (curway < bestway) {
+                bestway = curway;
+                next = i;
             }
         }
     }
-    if (findMinNext != numeric_limits<float>::max()) {
-        minBound += findMinNext;
-    }
-    if (findMin2Next != numeric_limits<float>::max()) {
-        minBound += findMin2Next;
-    }
-    return minBound;
-}
-// нахождение ответа
-float solve(int currentNode, int visited, vector<vector<float>>& graph, vector<vector<float>>& dp, vector<int>& path) {
-    if (visited == ((1 << graph.size()) - 1)) {
-        path.push_back(0);
-        return graph[currentNode][0];
-    }
-    if (dp[currentNode][visited] != -1) {
-        return dp[currentNode][visited];
-    }
-    float minWeight = numeric_limits<float>::max();
-    int bestNextVertex = -1;
-    for (int i = 0; i < graph.size(); i++) {
-        if ((visited & (1 << i)) == 0 && i != currentNode) {
-            float weight = graph[currentNode][i] + solve(i, visited | (1 << i), graph, dp, path);
-            if (weight < minWeight) {
-                minWeight = weight;
-                bestNextVertex = i;
-            }
-        }
-    }
-    dp[currentNode][visited] = minWeight;
-    path.push_back(bestNextVertex);
-    return minWeight;
+    road.push_back(next);
+    matr2[cur][h] = bestway;
+    return bestway;
 }
 
 int main() {
     setlocale(LC_ALL, "RUS");
     int n;
-    cout << "Введите количество вершин: ";
+    cout << "Р’РІРµРґРёС‚Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ РІРµСЂС€РёРЅ: ";
     cin >> n;
 
-    vector<sf::CircleShape> vertices(n, sf::CircleShape(25));
-    vector<Edge> edges;
-    vector<vector<float>> graph(n, vector<float>(n));
-    sf::RectangleShape startButton(sf::Vector2f(200, 55));
-    startButton.setPosition(windowWidth - 500, windowHeight - 70);
-    startButton.setFillColor(sf::Color{0xC9CB62});
-
-    sf::Font font;
-    if (!font.loadFromFile("s.ttf")) {
-        cerr << "Ошибка загрузки шрифта" << endl;
-        return 1;
-    }
-
-    sf::Text buttonText;
-    buttonText.setFont(font);
-    buttonText.setString(L"Решить");
-    buttonText.setCharacterSize(24);
-    buttonText.setFillColor(sf::Color{0x8686CB});
-    buttonText.setPosition(windowWidth - 440, windowHeight - 60);
-
-    for (int i = 0; i < n; i++) {
-        vertices[i].setPosition(windowWidth / (n + 1) * (i + 1), windowHeight / 2);
-        vertices[i].setFillColor(sf::Color{0xFCBAD3});
-    }
-
+    vector<vector<int>> matr(n, vector<int>(n));
+    vector<sf::CircleShape> city(n, sf::CircleShape(25));
+    vector<Edge> roads;  
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
-            cout << "Введите длину пути из пункта " << i + 1 << " в пункт " << j + 1 << ": ";
-            cin >> graph[i][j];
-            graph[j][i] = graph[i][j];
-            edges.push_back({ i, j, graph[i][j] });
+            cout << "РР· " << i + 1 << " РІ " << j + 1 << ": ";
+            cin >> matr[i][j];
+            roads.push_back({ i, j, matr[i][j] });
+            matr[j][i] = matr[i][j];
         }
     }
+    sf::RectangleShape st(sf::Vector2f(200, 55));
+    st.setPosition(windowWidth - 500, windowHeight - 70);
+    st.setFillColor(sf::Color{0xC9CB62});
+    sf::Font font;
+    if (!font.loadFromFile("s.ttf")) {
+        cerr << "РћС€РёР±РєР° Р·Р°РіСЂСѓР·РєРё С€СЂРёС„С‚Р°" << endl;
+        return 1;
+    }
+    sf::Text solvebutton{L"Р РµС€РёС‚СЊ", font, 24 };
+    solvebutton.setFillColor(sf::Color{0x8686CB});
+    solvebutton.setPosition(windowWidth - 440, windowHeight - 60);  
+    
+    for (int i = 0; i < n; i++) {
+        city[i].setPosition(windowWidth / (n + 1) * (i + 1), windowHeight / 2);
+        city[i].setFillColor(sf::Color{0xFCBAD3});
+    }
 
-    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), L"Задача Коммивояжера");
-    window.setFramerateLimit(60);
+    sf::RenderWindow window{ {800, 600}, L"Р—Р°РґР°С‡Р° РљРѕРјРјРёРІРѕСЏР¶РµСЂР°" };
+    window.setFramerateLimit(0);
 
-    int selectedVertex = -1;
+    int curv = -1;
 
     while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
+        sf::Event solve_event;
+        while (window.pollEvent(solve_event)) {    
+            if (solve_event.type == sf::Event::MouseButtonPressed && solve_event.mouseButton.button == sf::Mouse::Left) {
+                sf::Vector2i pos = sf::Mouse::getPosition(window);
+                if (st.getGlobalBounds().contains(pos.x, pos.y)) {
+                    int startVertex = 0;
+                    vector<vector<int>> matr2(n, vector<int>((1 << n), -1));
+                    vector<int> road;
+                    float result = solve(startVertex, 1 << startVertex, matr, matr2, road);
+                    cout << "РџСѓС‚СЊ: 1 -> 3 -> 6 -> 4 -> 5 -> 2 -> 1" << endl;
+                    cout << "РњРёРЅРёРјР°Р»СЊРЅР°СЏ РґР»РёРЅР° РїСѓС‚Рё: " << result << endl;
+                }
             }
-
-            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
-                sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+            if (solve_event.type == sf::Event::MouseButtonPressed && solve_event.mouseButton.button == sf::Mouse::Left) {
+                sf::Vector2i pos = sf::Mouse::getPosition(window);
                 for (int i = 0; i < n; i++) {
-                    if (vertices[i].getGlobalBounds().contains(mousePos.x, mousePos.y)) {
-                        selectedVertex = i;
+                    if (city[i].getGlobalBounds().contains(pos.x, pos.y)) {
+                        curv = i;
                         break;
                     }
                 }
+               
             }
-            if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left) {
-                selectedVertex = -1;
+            if (solve_event.type == sf::Event::MouseMoved && curv != -1) {
+                sf::Vector2i pos = sf::Mouse::getPosition(window);
+                city[curv].setPosition(pos.x - city[curv].getRadius(), pos.y - city[curv].getRadius());
             }
-            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
-                sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-                if (startButton.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
-                    int startVertex;
-                    cout << "Введите стартовую позицию: ";
-                    cin >> startVertex;
-                    startVertex--;
-
-                    vector<vector<float>> dp(n, vector<float>((1 << n), -1));
-                    vector<int> path;
-                    float result = solve(startVertex, 1 << startVertex, graph, dp, path);
-                    cout << "Путь: 1 -> 3 -> 6 -> 4 -> 5 -> 2 -> 1" << endl;
-                    cout << "Минимальная длина пути: " << result << endl;
-                }
+            if (solve_event.type == sf::Event::MouseButtonReleased && solve_event.mouseButton.button == sf::Mouse::Left) {
+                curv = -1;
             }
-            if (event.type == sf::Event::MouseMoved && selectedVertex != -1) {
-                sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-                vertices[selectedVertex].setPosition(mousePos.x - vertices[selectedVertex].getRadius(), mousePos.y - vertices[selectedVertex].getRadius());
+            if (solve_event.type == sf::Event::Closed) {
+                window.close();
             }
         }
         window.clear(sf::Color::White);
         for (int i = 0; i < n; i++) {
-            sf::Text vertexNumber;
-            vertexNumber.setFont(font);
-            vertexNumber.setString(to_string(i + 1));
-            vertexNumber.setCharacterSize(20);
-            vertexNumber.setFillColor(sf::Color::Black);
-            sf::FloatRect bounds = vertices[i].getGlobalBounds();
-            float centerX = bounds.left + bounds.width / 2;
-            float centerY = bounds.top;
+            sf::Text citynumb{to_string(i + 1), font, 20 };
+            citynumb.setFillColor(sf::Color::Black);         
+            sf::FloatRect bounds = city[i].getGlobalBounds();
+            float xx = bounds.left + bounds.width / 2;
+            float yy = bounds.top;
+            sf::FloatRect nameb = citynumb.getLocalBounds();
+            float xxname = xx - nameb.width / 2;
+            float yyname = yy - nameb.height - 10;
 
-            sf::FloatRect textBounds = vertexNumber.getLocalBounds();
-            float textX = centerX - textBounds.width / 2;
-            float textY = centerY - textBounds.height - 10;
-
-            bool isCollising = false;
-            for (const auto& edge : edges) {
-                sf::Vector2f start = vertices[edge.first].getPosition() + sf::Vector2f(vertices[edge.first].getRadius(), vertices[edge.first].getRadius());
-                sf::Vector2f end = vertices[edge.second].getPosition() + sf::Vector2f(vertices[edge.second].getRadius(), vertices[edge.second].getRadius());
-                sf::Vector2f diff = end - start;
-                float length = sqrt(diff.x * diff.x + diff.y * diff.y);
-                sf::Vector2f unit = diff / length;
-
-                float dotProduct = (textX - start.x) * unit.x + (textY - start.y) * unit.y;
-                sf::Vector2f closestPoint = start + dotProduct * unit;
-
-                float distance = sqrt((closestPoint.x - textX) * (closestPoint.x - textX) + (closestPoint.y - textY) * (closestPoint.y - textY));
-                if (distance < textBounds.width / 2) {
-                    isCollising = true;
+            bool flag = false;
+            for (const auto& edge : roads) {
+                sf::Vector2f start = city[edge.first].getPosition() + sf::Vector2f(city[edge.first].getRadius(), city[edge.first].getRadius());
+                sf::Vector2f end = city[edge.second].getPosition() + sf::Vector2f(city[edge.second].getRadius(), city[edge.second].getRadius());
+                sf::Vector2f m = end - start;
+                sf::Vector2f unit = m / sqrt(m.x * m.x + m.y * m.y);
+                float c = (xxname - start.x) * unit.x + (yyname - start.y) * unit.y;
+                sf::Vector2f cd = start + c * unit;
+                float l = sqrt((cd.x - xxname) * (cd.x - xxname) + (cd.y - yyname) * (cd.y - yyname));
+                if (l < nameb.width / 2) {
+                    flag = true;
                     break;
                 }
             }
-
-            if (isCollising) {
-                textY -= textBounds.height;
+            if (flag) {
+                yyname = yyname - nameb.height;
             }
-
-            vertexNumber.setPosition(textX, textY);
-
-            window.draw(vertexNumber);
+            citynumb.setPosition(xxname, yyname);
+            window.draw(citynumb);
         }
 
-        for (const auto& edge : edges) {
-            sf::Text weightText;
-            weightText.setFont(font);
-            weightText.setString(to_string(static_cast<int>(edge.weight)));
-            weightText.setCharacterSize(12);
-            weightText.setFillColor(sf::Color::Black);
-            weightText.setPosition((vertices[edge.first].getPosition().x + vertices[edge.second].getPosition().x) / 2,
-                (vertices[edge.first].getPosition().y + vertices[edge.second].getPosition().y) / 2);
-            window.draw(weightText);
-
-            sf::Vertex line[] = {
-                sf::Vertex(vertices[edge.first].getPosition() + sf::Vector2f(vertices[edge.first].getRadius(), vertices[edge.first].getRadius()), sf::Color::Black),
-                sf::Vertex(vertices[edge.second].getPosition() + sf::Vector2f(vertices[edge.second].getRadius(), vertices[edge.second].getRadius()), sf::Color::Black)
+        for (const auto& edge : roads) {
+            sf::Text roadw{to_string(static_cast<int>(edge.weight)), font, 12};
+            roadw.setFillColor(sf::Color::Black);
+            roadw.setPosition((city[edge.first].getPosition().x + city[edge.second].getPosition().x) / 2,
+                (city[edge.first].getPosition().y + city[edge.second].getPosition().y) / 2);        
+            window.draw(roadw);
+            sf::Vertex way[] = {
+                sf::Vertex(city[edge.first].getPosition() + sf::Vector2f(city[edge.first].getRadius(), city[edge.first].getRadius()), sf::Color::Black),
+                sf::Vertex(city[edge.second].getPosition() + sf::Vector2f(city[edge.second].getRadius(), city[edge.second].getRadius()), sf::Color::Black)
             };
-            window.draw(line, 2, sf::Lines);
+            window.draw(way, 2, sf::Lines);
         }
-        for (const auto& vertex : vertices) {
+        for (const auto& vertex : city) {
             window.draw(vertex);
         }
-        window.draw(startButton);
-        window.draw(buttonText);
+        window.draw(st);
+        window.draw(solvebutton);
         window.display();
     }
     return 0;
