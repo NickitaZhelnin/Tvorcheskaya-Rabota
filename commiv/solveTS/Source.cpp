eed603a75f62a3fd81d459dfd1f567fa46d9023c
Nickita Zhelnin <130294527+NickitaZhelnin@users.noreply.github.com>
2023-06-12 22:33:48 +0500

    Update Source.cpp

--- commiv/solveTS/Source.cpp
+++ commiv/solveTS/Source.cpp
@@ -1,52 +1,28 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
 #include <vector>
+#include <list>
+#include <SFML/Graphics.hpp>
 using namespace std;
 
 const int windowWidth = 800;
 const int windowHeight = 600;
 
-// путь из одного города в другой
 struct Edge {
     int first;
     int second;
     int weight;
 };
+int m = 1000;
+int n = 6;
+vector<vector<int>> matr(n, vector<int>(n));
+vector<vector<int>> matr2;
+vector<vector<int>> matr3;
+vector<pair<int, int>> way;
+vector<sf::CircleShape> city(n, sf::CircleShape(25));
+vector<Edge> roads;
 
-// нахождение ответа
-int solve(int cur, int h, vector<vector<int>>& matr, vector<vector<int>>& matr2, vector<int>& road) {
-    int next = -1;
-    if (matr2[cur][h] != -1) {
-        return matr2[cur][h];
-    }
-    if (h == ((1 << matr.size()) - 1)) {
-        road.push_back(0);
-        return matr[cur][0];
-    }  
-    int bestway = 999999;
-    for (int i = 0; i < matr.size(); i++) {
-        if ((h & (1 << i)) == 0 && i != cur) {
-            int curway = matr[cur][i] + solve(i, h | (1 << i), matr, matr2, road);
-            if (curway < bestway) {
-                bestway = curway;
-                next = i;
-            }
-        }
-    }
-    road.push_back(next);
-    matr2[cur][h] = bestway;
-    return bestway;
-}
 
-int main() {
-    setlocale(LC_ALL, "RUS");
-    int n;
-    cout << "Введите количество вершин: ";
-    cin >> n;
-
-    vector<vector<int>> matr(n, vector<int>(n));
-    vector<sf::CircleShape> city(n, sf::CircleShape(25));
-    vector<Edge> roads;  
+void create() {
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             cout << "Из " << i + 1 << " в " << j + 1 << ": ";
@@ -55,6 +31,169 @@ int main() {
             matr[j][i] = matr[i][j];
         }
     }
+}
+
+int rMin(int h, int g)
+{
+    int min = m;
+    for (int i = 0; i < n; i++)
+    {
+        if (matr2[h][i] < min && i != g && matr2[h][i] != -1)
+            min = matr2[h][i];
+    }
+    return min;
+}
+int cMin(int h, int g)
+{
+    int min = m;
+    for (int i = 0; i < n; i++)
+    {
+        if (matr2[i][g] < min && i != h && matr2[i][g] != -1)
+            min = matr2[i][g];
+    }
+    return min;
+}
+void fillingmatr() {
+    vector<int> tmp;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            if (i == j)
+                tmp.push_back(-1);
+            else
+                tmp.push_back(matr[i][j]);
+        matr2.push_back(tmp);
+        tmp.clear();
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            tmp.push_back(matr[i][j]);
+        matr3.push_back(tmp);
+        tmp.clear();
+    }
+}
+pair<int, int> bestways()
+{
+    int max = -m;
+    vector<vector < int>> copy = matr2;
+    std::pair<int, int> way;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (matr2[i][j] == 0)
+            {
+                int mark = 0;
+                mark += rMin(i, j);
+                mark += cMin(i, j);
+                if (max < mark)
+                {
+                    max = mark;
+                    way.first = i;
+                    way.second = j;
+                }
+            }
+        }
+    }
+    if (matr2[way.second][way.first] == 0)
+        matr2[way.second][way.first] = -1;
+    for (int i = 0; i < n; i++)
+    {
+        matr2[way.first][i] = -1;
+        matr2[i][way.second] = -1;
+    }
+    return way;
+}
+void Solve()
+{
+    matr2 = matr3;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+                matr2[i][j] = -1;
+            if (matr2[i][j] <= 0)
+                matr2[i][j] = -1;
+        }
+
+    int i = 0;
+    while (i != n) {  
+        std::pair<int, int> tmp;
+        for (int i = 0; i < n; i++)
+        {
+            int min = m;
+            for (int j = 0; j < n; j++)
+            {
+                if (matr2[j][i] < min && matr2[j][i] != -1)
+                    min = matr2[j][i];
+            }
+            if (min != m && min != 0)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matr2[j][i] > 0 && matr2[j][i] != m)
+                        matr2[j][i] -= min;
+                }
+            }
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            int min = m;
+            for (int j = 0; j < n; j++)
+            {
+                if (matr2[i][j] < min && matr2[i][j] != -1)
+                    min = matr2[i][j];
+            }
+            if (min != m && min != 0)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matr2[i][j] > 0 && matr2[i][j] != m)
+                        matr2[i][j] -= min;
+                }
+            }
+        }      
+        tmp = bestways();
+        if (tmp != std::pair<int, int>(0, 0))
+            way.push_back(tmp);
+        i++;
+    }
+}
+
+void bestwayprint() {
+    cout << "Путь: ";
+    int startpos = 0;
+    int sum = 0;
+    for (int i = 0; i < way.size(); i++) {
+        for (int j = 0; j < way.size(); j++) {
+            if (way[j].first == startpos) {
+                cout << way[j].first + 1 << " -> ";
+                startpos = way[j].second;
+                break;
+            }
+        }
+    }
+    bool f = false;
+    for (int j = 0; j < way.size(); j++)
+        if (way[j].first == startpos) {
+            cout << way[j].first + 1;
+            f = true;
+            break;
+        }
+    if (!f) {
+        for (int j = 0; j < way.size(); j++)
+            if (way[j].second == startpos)
+                cout << way[j].second + 1;
+    }
+    cout << endl;
+    for (int i = 0; i < way.size(); i++)
+        sum += matr[way[i].first][way[i].second];
+    cout << "Ответ: " << sum << endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS"); 
+    
     sf::RectangleShape st(sf::Vector2f(200, 55));
     st.setPosition(windowWidth - 500, windowHeight - 70);
     st.setFillColor(sf::Color{0xC9CB62});
@@ -83,12 +222,10 @@ int main() {
             if (solve_event.type == sf::Event::MouseButtonPressed && solve_event.mouseButton.button == sf::Mouse::Left) {
                 sf::Vector2i pos = sf::Mouse::getPosition(window);
                 if (st.getGlobalBounds().contains(pos.x, pos.y)) {
-                    int startVertex = 0;
-                    vector<vector<int>> matr2(n, vector<int>((1 << n), -1));
-                    vector<int> road;
-                    float result = solve(startVertex, 1 << startVertex, matr, matr2, road);
-                    cout << "Путь: 1 -> 3 -> 6 -> 4 -> 5 -> 2 -> 1" << endl;
-                    cout << "Минимальная длина пути: " << result << endl;
+                    create();
+                    fillingmatr();
+                    Solve();
+                    bestwayprint();
                 }
             }
             if (solve_event.type == sf::Event::MouseButtonPressed && solve_event.mouseButton.button == sf::Mouse::Left) {
@@ -117,9 +254,11 @@ int main() {
             sf::Text citynumb{to_string(i + 1), font, 20 };
             citynumb.setFillColor(sf::Color::Black);         
             sf::FloatRect bounds = city[i].getGlobalBounds();
+            float xx = bounds.left + bounds.width / 2;
+            float yy = bounds.top;
             sf::FloatRect nameb = citynumb.getLocalBounds();
-            float xtxt = bounds.left + bounds.width / 2 - nameb.width / 2;
-            float ytxt = bounds.top - nameb.height - 10;
+            float xxname = xx - nameb.width / 2;
+            float yyname = yy - nameb.height - 10;
 
             bool flag = false;
             for (const auto& edge : roads) {
